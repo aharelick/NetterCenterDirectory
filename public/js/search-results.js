f6b0daa7cee223121c8474187c5af213e6d2b1d7
anjalikhetan <anjali.khetan27@gmail.com>
2015-04-08 18:18:32 -0400

    beginning loading searches

--- public/js/search-results.js
+++ public/js/search-results.js
@@ -1,1 +1,9 @@
 //does not yet account for longhand of abreviations 
+
+	$(document).ready(function() {
+  		var results = (<%-JSON.stringify(results)%>);
+  		for (Object o : results) {
+  			console.log(o.bio);
+  		}
+
+	});
