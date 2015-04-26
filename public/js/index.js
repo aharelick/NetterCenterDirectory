5234094420bbc9cabfb01cd5ea60f602c558fc8e
mark345 <mjharding3@gmail.com>
2015-04-26 17:18:38 -0400

    homepage finished
    except for flash error font

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,16 @@
 $(document).ready(function () {
-	function toggle_div_slide(div) {
-		// if div is hidden, slide it down. otherwise, hide it
-		if ($(div).is(":hidden")) {
-			$(div).slideDown(450);
-			if (div == "#login-div") {
-				$("#create-div").slideUp(450);
-			}
-			else if (div == "#create-div") {
-				$("#login-div").slideUp(450);
-			}
+
+	$('#login-button').click(function() {
+		if ($('#login-div').is(":hidden")) {
+			$('#login-button').hide();
+			$('#login-div').slideDown(450);
+			$('html, body').animate({
+        		scrollTop: $("#login-div").offset().top
+    		}, 1000);
+		} else {
+			$('#login-div').slideUp(450);
 		}
-		else {
-			$(div).slideUp(450);
-		}
-	}
-	
-	$("#login-button").click(function() {
-		toggle_div_slide("#login-div");
-	});
-	
-	$("#create-button").click(function() {
-		toggle_div_slide("#create-div");
-	});
+	})
 	
 	// Another way to do the above stuff in one shot
 	// When any element that has an id that ends in button is clicked...
